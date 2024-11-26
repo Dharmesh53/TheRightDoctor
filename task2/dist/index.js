70f6b55b0b26fb58b72ea30d252d6079837e4274
Dharmesh53 <dharmeshwr@gmail.com>
2024-11-26 06:15:31 +0530

    fix: production file imports fixes

--- task2/dist/index.js
+++ task2/dist/index.js
@@ -1,15 +1,15 @@
 import "dotenv/config";
 import http from "http";
-import Router, { attachRoutes } from "./router";
-import connectToDB from "./db";
+import Router, { attachRoutes } from "./router/index.js";
+import connectToDB from "./db/index.js";
 const router = Router();
 attachRoutes(router);
 const server = http.createServer((req, res) => {
-    router.handle(req, res);
+  router.handle(req, res);
 });
 const startServer = () => {
-    server.listen(process.env.PORT, () => {
-        console.log(`Server is runnning on port ${process.env.PORT}`);
-    });
+  server.listen(process.env.PORT, () => {
+    console.log(`Server is runnning on port ${process.env.PORT}`);
+  });
 };
 connectToDB().then(startServer);
